f8a5acbd12929b2a4859b9b738bbaa0abaea51aa
wiczek2 <wiczek2@poczta.onet.pl>
2020-04-21 15:05:07 +0200

    Trigger

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times. And I was changed.\n'.format(count)
+    return 'Hello World! I have been seen {} times. And I was changed again.\n'.format(count)
